71bc1953fb589a14a1c2cee64a7017672d800ab5
sunecosuri <sunecosuri@gmail.com>
2019-01-15 17:53:19 +0900

    res.header に Access-Control-Alllow-* を追加

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 connection.init();
 routes.configure(app);
 
