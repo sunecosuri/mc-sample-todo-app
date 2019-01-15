abb87beb8dc519f96d15298f0c50f9b8c109ccf4
sunecosuri <sunecosuri@gmail.com>
2019-01-15 17:59:12 +0900

    置換する文字列を指定する

--- models/task.js
+++ models/task.js
@@ -12,16 +12,16 @@ function Task() {
 
   this.get = function(id, res) {
     connection.acquire(function (err, con) {
-      con.query('SELECT * FROM tasks WHERE id = ?', [id], function (err, result) {
+      con.query('SELECT * FROM tasks WHERE id = $1', [id], function (err, result) {
         con.release();
         res.send(result);
       });
     });
   };
 
-  this.create = function(task, res) {
+  this.create = function(title, order, res) {
     connection.acquire(function(err, con) {
-      con.query('INSERT INTO tasks SET ?', task, function(err, result) {
+      con.query('INSERT INTO todos ("title", "order", "completed") VALUES ($1, $2, false) RETURNING *', [title, order], function(err, result) {
         con.release();
         if (err) {
           res.send({status: 1, message: 'Task creation failed'});
@@ -34,7 +34,7 @@ function Task() {
 
   this.update = function(task, res) {
     connection.acquire(function(err, con) {
-      con.query('UPDATE tasks SET ? WHERE ID = ?', [task, task.id], function(err, result) {
+      con.query('UPDATE tasks SET $1 WHERE ID = $2', [task, task.id], function(err, result) {
         con.release();
         if (err) {
           res.send({status: 1, message: 'Task update failed'});
@@ -47,7 +47,7 @@ function Task() {
 
   this.delete = function(id, res) {
     connection.acquire(function(err, con) {
-      con.query('DELTE FROM tasks WHERE id = ?', [id], function(err, result) {
+      con.query('DELTE FROM tasks WHERE id = $1', [id], function(err, result) {
         con.release();
         if (err) {
           res.send({status: 1, message: 'Failed to delete'});
